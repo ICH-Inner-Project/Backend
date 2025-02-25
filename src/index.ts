f7f923dea1a8db3e8935a55a9875879f44761167
Bingine <alex.10.03.2006@gmail.com>
2025-02-25 16:12:42 +0100

    added the graphql endpoint + ruru ide

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import connectDB from '../src/db/init';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-const PORT = process.env.PORT || 3000;
+const { ruruHTML } = require('ruru/server');
+
+import fullHandler from "@graphql/handler";
+import connectDB from '@db/init';
 
 connectDB();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+
+app.all('/graphql', fullHandler);
+
+app.get('/', (_req, res) => {
+  res.type('html');
+  res.end(ruruHTML({ endpoint: '/graphql' }));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
