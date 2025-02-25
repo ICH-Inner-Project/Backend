7aa98e2bc9d0b3e6cb55c71f8cbae4ce2fb22073
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2025-02-25 22:29:24 +0100

    new connection

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 import 'dotenv/config'
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI || 'mongodb://mongo.edu.itcareerhub.de:27017';
 const connectDB = async (): Promise<void> => {
   try {
     await mongoose.connect(uri || '', {
-      user: process.env.DB_USER || 'root',
-      pass: process.env.DB_PASS || '666777lyly',
-      dbName: process.env.DB_NAME || 'ich_prod',
+      user: process.env.DB_USER || 'ich_editor',
+      pass: process.env.DB_PASS || 'verystrongpassword',
+      dbName: process.env.DB_NAME || 'ich_edit',
+      authSource: 'ich_edit',
+      readPreference: 'primary',
+      ssl: false,
+      authMechanism: 'DEFAULT',
     });
     console.log('Connected to Mongo DB');
   } catch (error) {
