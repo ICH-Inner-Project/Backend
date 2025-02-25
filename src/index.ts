b949b43248741f2e50e6d8980dc5fcf78e50d2a3
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2025-02-25 15:01:34 +0100

    server.js added middleware and conect to db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import express from "express";
-import  "dotenv/config";
-
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from '../src/db/init';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT;
+connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
