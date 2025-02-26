c931a8ddb651647b33adaf99d7c8a3f968a4115b
Bingine <alex.10.03.2006@gmail.com>
2025-02-26 08:55:44 +0100

    fixed mongo modles

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -1,17 +1,20 @@
-import { Schema, model, Document } from 'mongoose';
-import { hashPassword, comparePassword } from '../../utils/bcrypt';
+import { Schema, model, Document, Types } from 'mongoose';
 
-interface IUser extends Document {
+export enum Roles {
+    user = "user",
+    admin = "admin",
+}
+
+export interface IUser extends Document {
   username: string;
   password: string;
   phone: string;
   birthday: Date;
   gender: string;
-  name: string;
-  posts: string[];
-  admin: boolean;
-  createAt: Date;
-  comparePassword(password: string): Promise<boolean>;
+  firstName: string;
+  lastName: string;
+  posts: Types.ObjectId[];
+  role: string;
 }
 
 const userSchema = new Schema<IUser>({
@@ -20,24 +23,13 @@ const userSchema = new Schema<IUser>({
   phone: { type: String, trim: true, required: true },
   birthday: { type: Date, trim: true, required: true },
   gender: { type: String, trim: true, required: true },
-  name: { type: String, trim: true, required: true },
-  posts: [{ type: String, required: true }],
-  admin: { type: Boolean, required: true },
-  createAt: {
-    type: Date,
-    default: Date.now,
-  },
+  firstName: { type: String, trim: true, required: true },
+  lastName: { type: String, trim: true, required: true },
+  posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  role: { type: String, trim: true, required: true },
+}, {
+    timestamps: true,
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await hashPassword(this.password);
-});
+export const User = model<IUser>('User', userSchema);
 
-userSchema.methods.comparePassword = async function (password: string) {
-  return await comparePassword(password, this.password);
-};
-
-const User = model<IUser>('User', userSchema);
-
-export default User;
